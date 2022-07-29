7b65511c6e135c0bcadd16ad946bbd298eb26e8f
Afsal102 <39826977+Afsal102@users.noreply.github.com>
2022-07-30 00:14:41 +0530

    feat: adds api constants - once dev is complete update this commit

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  //TODO(afzal): update to production Uri
+  static const deeplinkDomainUri = 'https://conveneapp.page.link';
+  static const deeplinkDataDomainUri = 'https://www.tadaspetra.com';
+}
