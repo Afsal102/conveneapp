8c5a73b3ecedb920a642752d4042f52a4acdb69c
Afsal102 <39826977+Afsal102@users.noreply.github.com>
2022-07-31 19:10:25 +0530

    chore: updates deeplink domain uri to production uri - updates manifest scheme to match the deep link domain

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,6 +2,6 @@ class ApiConstants {
   ApiConstants._();
 
   //TODO(afzal): update to production Uri
-  static const deeplinkDomainUri = 'https://conveneapp.page.link';
+  static const deeplinkDomainUri = 'https://convene.page.link';
   static const deeplinkDataDomainUri = 'https://www.tadaspetra.com';
 }
